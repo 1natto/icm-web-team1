7589727ec41b7f543f0e00cf1415d5bf70526aee
Nguyen Hoang Phu <108948401+1natto@users.noreply.github.com>
2023-02-17 19:41:39 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, Response
 import os
-from datetime import datetime
 
 app = Flask(__name__)
 
@@ -8,25 +7,26 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/submit", methods=['GET'])
+@app.route("/draft1/", methods=['GET'])
 def submit():
-    name = request.args.get("name")
-    code = request.args.get("code")
-    folderpath = "./code"
-    prewritten_tag  = ".cpp"
-    user_filename = "chonphong"
-    round_name = "1"
-    filename = user_filename + prewritten_tag
-    fullpath = os.path.join(folderpath,filename)
-    filepath = os.path.join(folderpath,name)
-    if not os.path.exists(filepath):
-        os.makedirs(filepath)
-
-    with open(os.path.join(filepath, filename), "w") as f:
-        full_txt = code
-        f.write(full_txt)
-    with open(os.path.join(filepath, round_name), "w") as v:
-        txt = datetime.now()
-        v.write(str(txt))
     return render_template("submit.html")
 
+@app.route("/draft2/", methods=['GET'])
+def ny2():
+    return render_template("ny2.html")
+@app.route("/ny3/", methods=['GET'])
+def ny3():
+    return render_template("ny3.html")
+@app.route("/ny4/", methods=['GET'])
+def ny4():
+    return render_template("ny4.html")
+@app.route("/ny5/", methods=['GET'])
+def ny5():
+    return render_template("ny5.html")
+@app.route("/ny6/", methods=['GET'])
+def ny6():
+    return render_template("ny6.html")
+
+
+if __name__ == '__main__':
+   app.run()
